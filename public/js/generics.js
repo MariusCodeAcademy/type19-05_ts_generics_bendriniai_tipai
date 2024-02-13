d486877621dbddb3e3a782515ce48a97919bb321
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-13 08:59:05 +0200

    string

--- public/js/generics.js
+++ public/js/generics.js
@@ -1,9 +1,18 @@
 "use strict";
 console.log('generics');
 function printVal(x) {
-    console.log('x ===', x);
+    if (typeof x === 'string') {
+        console.log('x ===', x.toUpperCase());
+    }
+    else {
+        console.log('x ===', x);
+    }
     return x;
 }
 const sk = printVal(50);
+sk.toFixed(2);
 const str = printVal('labas');
+str.toLowerCase();
+const strArr = printVal(['blue', 'green', 'red']);
+console.log('strArr ===', strArr);
 //# sourceMappingURL=generics.js.map
--- src/generics.ts
+++ src/generics.ts
@@ -2,12 +2,25 @@ console.log('generics');
 
 // the problem
 
-function printVal(x: number | string): number | string {
-  console.log('x ===', x);
+// function printVal(x: number | string): number | string {
+//   console.log('x ===', x);
+//   return x;
+// }
+function printVal<T>(x: T): T {
+  if (typeof x === 'string') {
+    console.log('x ===', x.toUpperCase());
+  } else {
+    console.log('x ===', x);
+  }
   return x;
 }
 
-// sk nera skaiciaus tipo
-const sk = printVal(50);
-// str nera string tipo
-const str = printVal('labas');
+// yra skaiciaus tipo
+const sk = printVal<number>(50);
+sk.toFixed(2);
+// yra string tipo
+const str = printVal<string>('labas');
+str.toLowerCase();
+
+const strArr = printVal<string[]>(['blue', 'green', 'red']);
+console.log('strArr ===', strArr);
